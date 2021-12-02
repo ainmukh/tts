b1f99a4bbe142ff6baa1cc07449ffd0d941b3466
ainmukh <aynurmukh@gmail.com>
2021-12-02 21:23:31 +0300

    melspec fix

--- tts/trainer/trainer.py
+++ tts/trainer/trainer.py
@@ -162,6 +162,8 @@ class Trainer(BaseTrainer):
             for batch_idx, batch in enumerate(self.valid_data_loader):
                 # batch = self.move_batch_to_device(batch, self.device)
                 batch = batch.to(self.device)
+                melspec = self.melspec(batch.waveform)
+                batch.melspec = melspec
                 batch = self.model(batch)
 
                 audio = []
